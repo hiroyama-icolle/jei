21a7c7d64a6824e658914e9e6652ee33020571a3
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2024-06-04 15:07:01 +0900

    20241506

---
+++ d48Q6.java
@@ -0,0 +1,33 @@
+class d48Q6 {
+    public static void main(String[] args){
+    }
+}
+
+class Match {
+    Achievement teamA;
+    Achievement teamB;
+
+    void setAchievements(Achevement a, Achevement b){
+    }
+
+    String showResult(){
+    }
+
+    int winPoint(){
+    }
+
+    int matchPoint(){
+    }
+
+    int bonusPoint(){
+    }
+}
+
+class Achievement {
+    int point;
+    int tryNumber;
+
+    void set(){
+    }
+}
+
